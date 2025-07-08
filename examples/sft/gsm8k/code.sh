231b939255ba8396d9693ca23b4ef3cda4b6c9b2
root <root@bolt-a6zgxgh6dz-5jnbqte2kr.bolt-pods.turi-bolt.svc.cluster.local>
2025-07-08 15:06:05 +0000

    code parquet to s3

--- examples/sft/gsm8k/code.sh
+++ examples/sft/gsm8k/code.sh
@@ -7,8 +7,8 @@ export MASTER_PORT=29500
 
 # Shift the arguments so $@ refers to the rest
 shift 2
-#--standalone #--node_rank 0 --rdzv_id "my_experiment" --rdzv_backend c10d --rdzv_endpoint="${MASTER_ADDR}:${MASTER_PORT}"
-torchrun --standalone --nnodes=1 --nproc_per_node=$nproc_per_node \
+#--standalone #--node_rank $NODE_RANK --rdzv_id "my_experiment" --rdzv_backend c10d --rdzv_endpoint="${MASTER_ADDR}:${MASTER_PORT}"
+torchrun --nnodes=8 --nproc_per_node=$nproc_per_node --node_rank $NODE_RANK --rdzv_id "my_experiment" --rdzv_backend c10d --rdzv_endpoint="${MASTER_ADDR}:${MASTER_PORT}" \
      -m verl.trainer.fsdp_sft_trainer \
     data.train_files=s3://afm-common-permanent/shenao_zhang/opc_annealing_real_algo_corpus/traincode \
     data.val_files=s3://afm-common-permanent/shenao_zhang/opc_annealing_real_algo_corpus/testcode \
@@ -24,8 +24,8 @@ torchrun --standalone --nnodes=1 --nproc_per_node=$nproc_per_node \
     data.micro_batch_size_per_gpu=32 \
     model.partial_pretrain=ZhangShenao/Llama-3.2-1B \
     model.use_liger=True \
-    trainer.project_name=cpt-math \
-    trainer.experiment_name=cpt-code-llama3.2-1b-realdata \
+    trainer.project_name=cpt-code \
+    trainer.experiment_name=cpt-code-llama3.2-1b-realalgodata \
     trainer.logger=['console','wandb'] \
     trainer.default_hdfs_dir=null $@ \
     trainer.save_freq=2000 \
