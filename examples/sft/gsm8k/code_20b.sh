948c5df7d4c9012026a2ebeab3434875cdf58126
root <root@bolt-3i7bqedgex-5h5w8yz9bc.bolt-pods.turi-bolt.svc.cluster.local>
2025-07-24 01:46:51 +0000

    4 nodes

--- examples/sft/gsm8k/code_20b.sh
+++ examples/sft/gsm8k/code_20b.sh
@@ -2,13 +2,13 @@ set -x
 
 nproc_per_node=8
 
-export MASTER_ADDR=240.62.237.180
+#export MASTER_ADDR=240.62.237.180
 export MASTER_PORT=29500
 
 # Shift the arguments so $@ refers to the rest
 shift 2
 #--standalone #--node_rank $NODE_RANK --rdzv_id "my_experiment" --rdzv_backend c10d --rdzv_endpoint="${MASTER_ADDR}:${MASTER_PORT}"
-torchrun --nnodes=8 --nproc_per_node=$nproc_per_node --node_rank $NODE_RANK --rdzv_id "my_experiment" --rdzv_backend c10d --rdzv_endpoint="${MASTER_ADDR}:${MASTER_PORT}" \
+torchrun --nnodes=4 --nproc_per_node=$nproc_per_node --node_rank $NODE_RANK --rdzv_id "my_experiment" --rdzv_backend c10d --rdzv_endpoint="${MASTER_ADDR}:${MASTER_PORT}" \
      -m verl.trainer.fsdp_sft_trainer \
     data.train_files=s3://afm-common-permanent/shenao_zhang/small_20b_python_data/train \
     data.val_files=s3://afm-common-permanent/shenao_zhang/small_20b_python_data/test \
@@ -16,12 +16,12 @@ torchrun --nnodes=8 --nproc_per_node=$nproc_per_node --node_rank $NODE_RANK --rd
     data.max_length=8192 \
     data.train_batch_size=512 \
     data.truncation=right \
-    optim.lr=5e-5 \
+    optim.lr=2e-5 \
     optim.lr_scheduler=wsd \
     optim.weight_decay=0.1 \
     optim.warmup_steps_ratio=0 \
     +data.response_dict_keys=['text'] \
-    data.micro_batch_size_per_gpu=16 \
+    data.micro_batch_size_per_gpu=32 \
     model.partial_pretrain=ZhangShenao/Llama-3.2-3B \
     model.use_liger=True \
     trainer.project_name=llama-cpt \
